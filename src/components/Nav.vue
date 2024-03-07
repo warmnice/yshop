c81cb15b4cd1c343ac144544dd2972718613b4df
wujingnice <opeaky@outlook.com>
2024-03-07 10:13:38 +0800

    three

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -17,22 +17,22 @@
       </div>
     </div>
     <div class="tag">
-      <router-link v-show="tagShow[0]" to="/home/user" :class="{clickTag: tagClick[0]}" @click="resetTagClick(0)" class="start"><span>首页</span></router-link>
-      <router-link v-show="tagShow[1]" to="/home/decoration" :class="{clickTag: tagClick[1]}" @click="resetTagClick(1)"><span>终端装修</span><span @click="tagShow[0] = false">X</span></router-link>
-      <router-link v-show="tagShow[2]" to="/home/productm" :class="{clickTag: tagClick[2]}" @click="resetTagClick(2)"><span>商品管理</span><span @click="tagShow[1] = false">X</span></router-link>
-      <router-link v-show="tagShow[3]" to="/home/memberm" :class="{clickTag: tagClick[3]}" @click="resetTagClick(3)"><span>会员管理</span><span @click="tagShow[2] = false">X</span></router-link>
-      <router-link v-show="tagShow[4]" to="/home/orderm" :class="{clickTag: tagClick[4]}" @click="resetTagClick(4)"><span>订单管理</span><span @click="tagShow[3] = false">X</span></router-link>
-      <router-link v-show="tagShow[5]" to="/home/couponm" :class="{clickTag: tagClick[5]}" @click="resetTagClick(5)"><span>电子券管理</span><span @click="tagShow[4] = false">X</span></router-link>
-      <router-link v-show="tagShow[6]" to="/home/marketm" :class="{clickTag: tagClick[6]}" @click="resetTagClick(6)"><span>营销管理</span><span @click="tagShow[5] = false">X</span></router-link>
-      <router-link v-show="tagShow[7]" to="/home/distributionm" :class="{clickTag: tagClick[7]}" @click="resetTagClick(7)"><span>分销管理</span><span @click="tagShow[6] = false">X</span></router-link>
-      <router-link v-show="tagShow[8]" to="/home/moneym" :class="{clickTag: tagClick[8]}" @click="resetTagClick(8)"><span>财务管理</span><span @click="tagShow[7] = false">X</span></router-link>
-      <router-link v-show="tagShow[9]" to="/home/shopm" :class="{clickTag: tagClick[9]}" @click="resetTagClick(9)"><span>门店管理</span><span @click="tagShow[8] = false">X</span></router-link>
-      <router-link v-show="tagShow[10]" to="/home/integralm" :class="{clickTag: tagClick[10]}" @click="resetTagClick(10)"><span>积分管理</span><span @click="tagShow[9] = false">X</span></router-link>
-      <router-link v-show="tagShow[11]" to="/home/wxm" :class="{clickTag: tagClick[11]}" @click="resetTagClick(11)"><span>微信管理</span><span @click="tagShow[10] = false">X</span></router-link>
-      <router-link v-show="tagShow[12]" to="/home/shpos" :class="{clickTag: tagClick[12]}" @click="resetTagClick(12)"><span>商城配置</span><span @click="tagShow[11] = false">X</span></router-link>
-      <router-link v-show="tagShow[13]" to="/home/systemm" :class="{clickTag: tagClick[13]}" @click="resetTagClick(13)"><span>系统管理</span><span @click="tagShow[12] = false">X</span></router-link>
-      <router-link v-show="tagShow[14]" to="/home/smonitor" :class="{clickTag: tagClick[14]}" @click="resetTagClick(14)"><span>系统监控</span><span @click="tagShow[13] = false">X</span></router-link>
-      <router-link v-show="tagShow[15]" to="/home/stool" :class="{clickTag: tagClick[15]}" @click="resetTagClick(15)"><span>系统工具</span><span @click="tagShow[14] = false">X</span></router-link>
+      <router-link v-show="tagShow[0]" to="/home/user"  :class="{clickTag: tagClick[0]}" class="start"><span @click="homeStore.resetTagClick(0)">首页</span></router-link>
+      <router-link v-show="tagShow[1]" to="/home/decoration" :class="{clickTag: tagClick[1]}"><span @click="homeStore.resetTagClick(1)">终端装修</span><span @click="homeStore.showRightTag(1)">X</span></router-link>
+      <router-link v-show="tagShow[2]" to="/home/productm" :class="{clickTag: tagClick[2]}"><span @click="homeStore.resetTagClick(2)">商品管理</span><span @click="homeStore.showRightTag(2)">X</span></router-link>
+      <router-link v-show="tagShow[3]" to="/home/memberm" :class="{clickTag: tagClick[3]}"><span @click="homeStore.resetTagClick(3)">会员管理</span><span @click="homeStore.showRightTag(3)">X</span></router-link>
+      <router-link v-show="tagShow[4]" to="/home/orderm" :class="{clickTag: tagClick[4]}"><span @click="homeStore.resetTagClick(4)">订单管理</span><span @click="homeStore.showRightTag(4)">X</span></router-link>
+      <router-link v-show="tagShow[5]" to="/home/couponm" :class="{clickTag: tagClick[5]}"><span @click="homeStore.resetTagClick(5)">电子券管理</span><span @click="homeStore.showRightTag(5)">X</span></router-link>
+      <router-link v-show="tagShow[6]" to="/home/marketm" :class="{clickTag: tagClick[6]}"><span @click="homeStore.resetTagClick(6)">营销管理</span><span @click="homeStore.showRightTag(6)">X</span></router-link>
+      <router-link v-show="tagShow[7]" to="/home/distributionm" :class="{clickTag: tagClick[7]}"><span @click="homeStore.resetTagClick(7)">分销管理</span><span @click="homeStore.showRightTag(7)">X</span></router-link>
+      <router-link v-show="tagShow[8]" to="/home/moneym" :class="{clickTag: tagClick[8]}"><span @click="homeStore.resetTagClick(8)">财务管理</span><span @click="homeStore.showRightTag(8)">X</span></router-link>
+      <router-link v-show="tagShow[9]" to="/home/shopm" :class="{clickTag: tagClick[9]}"><span @click="homeStore.resetTagClick(9)">门店管理</span><span @click="homeStore.showRightTag(9)">X</span></router-link>
+      <router-link v-show="tagShow[10]" to="/home/integralm" :class="{clickTag: tagClick[10]}"><span @click="homeStore.resetTagClick(10)">积分管理</span><span @click="homeStore.showRightTag(10)">X</span></router-link>
+      <router-link v-show="tagShow[11]" to="/home/wxm" :class="{clickTag: tagClick[11]}"><span @click="homeStore.resetTagClick(11)">微信管理</span><span @click="homeStore.showRightTag(11)">X</span></router-link>
+      <router-link v-show="tagShow[12]" to="/home/shpos" :class="{clickTag: tagClick[12]}"><span @click="homeStore.resetTagClick(12)">商城配置</span><span @click="homeStore.showRightTag(12)">X</span></router-link>
+      <router-link v-show="tagShow[13]" to="/home/systemm" :class="{clickTag: tagClick[13]}"><span @click="homeStore.resetTagClick(13)">系统管理</span><span @click="homeStore.showRightTag(13)">X</span></router-link>
+      <router-link v-show="tagShow[14]" to="/home/smonitor" :class="{clickTag: tagClick[14]}"><span @click="homeStore.resetTagClick(14)">系统监控</span><span @click="homeStore.showRightTag(14)">X</span></router-link>
+      <router-link v-show="tagShow[15]" to="/home/stool" :class="{clickTag: tagClick[15]}"><span @click="homeStore.resetTagClick(15)">系统工具</span><span @click="homeStore.showRightTag(15)">X</span></router-link>
     </div>
   </nav>
 </template>
@@ -43,8 +43,9 @@ import { useHomeStore } from '../store/home'
 import { storeToRefs } from 'pinia'
 import screenfull from 'screenfull'
 
-const store = useHomeStore()
-const { fold, full } = storeToRefs(store)
+const homeStore = useHomeStore()
+const { fold, full, tagShow, tagClick } = storeToRefs(homeStore)
+tagClick.value[0] = true
 const title = ref('首页')
 function fullScreen () {
   if (screenfull.isEnabled && !screenfull.isFullscreen) {
@@ -57,15 +58,6 @@ function exitFullScreen () {
     screenfull.exit()
     full.value = false
   }
-}
-const tagShow = ref(Array.from({ length: 15 }, () => true))
-const tagClick = ref(Array.from({ length: 15 }, () => false))
-tagClick.value[0] = true
-function resetTagClick (index) {
-  for (let i = 0; i < tagClick.value.length; i++) {
-    tagClick.value[i] = false
-  }
-  tagClick.value[index] = true
 }
 </script>
 
--- src/store/home.js
+++ src/store/home.js
@@ -2,7 +2,32 @@ import { defineStore } from 'pinia'
 export const useHomeStore = defineStore('home', {
   state: () => ({
     fold: false,
-    full: false
-  })
-  // 下边可继续定义 getters, actions
+    full: false,
+    tagShow: Array.from({ length: 15 }, () => true),
+    tagClick: Array.from({ length: 15 }, () => false)
+  }),
+  actions: {
+    resetTagClick (index) {
+      for (let i = 0; i < this.tagClick.length; i++) {
+        this.tagClick[i] = false
+      }
+      this.tagShow[index] = true
+      this.tagClick[index] = true
+    },
+    showRightTag (index) {
+      this.tagShow[index] = false
+      if (!this.tagClick[index]) return
+      for (let i = 0; i < this.tagClick.length; i++) {
+        this.tagClick[i] = false
+      }
+      let i = index - 1
+      while (i >= 0) {
+        if (this.tagShow[i]) {
+          this.tagClick[i] = true
+          break
+        }
+        i--
+      }
+    }
+  }
 })
