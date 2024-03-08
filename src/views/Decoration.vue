5a9060408d1244c339e459cb59e220b064b72dbf
wujingnice <opeaky@outlook.com>
2024-03-08 20:16:26 +0800

    four

--- src/views/Decoration.vue
+++ src/views/Decoration.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <VueDraggable
-      ref="el"
-      v-model="list"
-      :disabled="disabled"
-      :animation="150"
-      ghostClass="ghost"
-    >
-      <div
-        v-for="item in list"
-        :key="item.id"
-        class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move"
-      >
-        <SlideShow :key="6" />
-        {{ item.name }}
+    <div class="decoration">
+      <div class="option">
+        <VueDraggable group="slide" class="list-group">
+          <div v-for="id in list" :key="id" class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move">
+            <SlideShow />
+          </div>
+        </VueDraggable>
       </div>
-    </VueDraggable>
+      <div class="draft">
+        <VueDraggable group="slide" class="list-group">
+          <div v-for="id in list" :key="id" class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move">
+            <SlideShow />
+          </div>
+        </VueDraggable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,32 +23,41 @@
 import { ref } from 'vue'
 import { VueDraggable } from 'vue-draggable-plus'
 import SlideShow from '@/components/SlideShow.vue'
-const list = ref([
-  {
-    name: 'Joao',
-    id: 1
-  },
-  {
-    name: 'Jean',
-    id: 2
-  },
-  {
-    name: 'Johanna',
-    id: 3
-  },
-  {
-    name: 'Juan',
-    id: 4
-  }
-])
 
-const el = ref()
-const disabled = ref(false)
+const list = ref([1, 2, 3, 4])
 </script>
 
-<style scoped>
-.ghost {
-  opacity: 0.5;
-  background: #c8ebfb;
+<style lang="scss" scoped>
+.decoration {
+  position: relative;
+  top: 5vh;
+  width: 85%;
+  height: 70vh;
+  margin: 0 auto;
+  padding: 1vw;
+  overflow: auto;
+  display: flex;
+  justify-content: space-between;
+  .option {
+    width: 20%;
+    height: 100%;
+  }
+  .draft {
+    width: 50%;
+    height: 100%;
+  }
+  &::-webkit-scrollbar {
+    width: 0.6vw;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 0.3vw;
+    -webkit-box-shadow: inset 0 0 5px rgba(154, 204, 213, 0.2);
+    background: #535353;
+  }
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    border-radius: 0.3vw;
+    background: #ededed;
+  }
 }
 </style>
